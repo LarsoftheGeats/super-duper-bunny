61ba41fed9e02145acf7bb4ab45de5528f6f8eb3
Jonathan Nielsen <larsnielsenj@gmail.com>
2022-10-19 17:10:08 -0600

    completed the swapi manipulation

---
+++ main.js
@@ -0,0 +1,25 @@
+//get the buttton we're working on
+const buttonPush = document.querySelector("button");
+const baseUrl= "https://swapi.dev/api/planets/?search=Alderaan"
+
+function buttonClick(evt){
+    //console.log("you pushed the button")
+    axios.get(baseUrl).then( (res) => 
+    {
+        //console.log(res)
+        for (let i=0; i< res.data.results[0].residents.length; i++){
+            let person=res.data.results[0].residents[i]
+            axios.get(person).then( (res1) => {
+                //console.log(res1)
+                let aldResident = res1.data.name;
+                let residentHTML = document.createElement("h2")
+                residentHTML.classList.add("nameClass")
+                residentHTML.textContent=aldResident;
+                document.body.appendChild(residentHTML)
+            } )
+        }
+        
+    })
+}
+
+buttonPush.addEventListener("click",buttonClick)
